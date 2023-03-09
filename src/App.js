2eadd701d4d5683df0c25e5f41b62dbe567e4597
maxrudyi <rudyimax@gmail.com>
2023-03-09 14:11:09 +0100

    v2.1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import FacebookLoginComponent from "./Pages/Components/FacebookLogin";
