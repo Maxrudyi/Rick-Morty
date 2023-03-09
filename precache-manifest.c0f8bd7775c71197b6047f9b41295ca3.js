a6ea907f3b32427977f76788b9328f37bae070d0
maxrudyi <rudyimax@gmail.com>
2023-03-09 14:31:15 +0100

    Updates

---
+++ precache-manifest.c0f8bd7775c71197b6047f9b41295ca3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a818d6b6b6f23286acc",
+    "url": "/Rick-Morty/static/css/main.cea42d46.chunk.css"
+  },
+  {
+    "revision": "4a818d6b6b6f23286acc",
+    "url": "/Rick-Morty/static/js/main.4a818d6b.chunk.js"
+  },
+  {
+    "revision": "071f6974c376b039043f",
+    "url": "/Rick-Morty/static/js/1.071f6974.chunk.js"
+  },
+  {
+    "revision": "e45ed7408fc165804f52",
+    "url": "/Rick-Morty/static/js/2.e45ed740.chunk.js"
+  },
+  {
+    "revision": "d2788bce093e8ca75424",
+    "url": "/Rick-Morty/static/js/runtime~main.d2788bce.js"
+  },
+  {
+    "revision": "300fb0524c4dc3a52728d51057c139f3",
+    "url": "/Rick-Morty/static/media/FacebookLogo.300fb052.svg"
+  },
+  {
+    "revision": "c1a417e7ec7b13cb8c09b2086b3e3553",
+    "url": "/Rick-Morty/static/media/RickAndMorty.c1a417e7.png"
+  },
+  {
+    "revision": "547219983926e6ad12da46e4a8f42a38",
+    "url": "/Rick-Morty/index.html"
+  }
+];
